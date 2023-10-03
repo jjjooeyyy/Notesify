7fc60c6c947d286fad00b05c72f4f0b9fd02e818
JOEY <puiying526@gmail.com>
2023-10-03 16:10:45 +0100

    Work on getting notes data on dashboard route

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -1,8 +1,27 @@
+const Note = require('../models/Notes');
+const mongoose = require('mongoose');
+
 // GET Dashboard
 exports.dashboard = async(req,res)=> {
+
     const locals = {
         title: "Dashboard",
         description: "Free Notes App"
     }
-    res.render('dashboard/index.ejs',{locals,layout:'../views/layouts/dashboard'})
+
+    try {
+      const notes = await Note.find({});
+
+      res.render('dashboard/index.ejs',{
+        userName:req.user.firstName,
+        locals,
+        notes,
+        layout:'../views/layouts/dashboard'})
+
+    } catch(error){
+      console.log(error);
+    }
+
+
+
 }
