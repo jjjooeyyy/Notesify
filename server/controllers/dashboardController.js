055095c32d86cdedc8b33c70fe39831355259317
JOEY <puiying526@gmail.com>
2023-10-04 13:40:01 +0100

    working on pagination

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -1,27 +1,45 @@
-const Note = require('../models/Notes');
-const mongoose = require('mongoose');
+const Note = require("../models/Notes");
+const mongoose = require("mongoose");
 
 // GET Dashboard
-exports.dashboard = async(req,res)=> {
+exports.dashboard = async (req, res) => {
 
-    const locals = {
-        title: "Dashboard",
-        description: "Free Notes App"
-    }
+    let perPage = 8;
+    let page = req.query.page || 1;
 
-    try {
-     const notes = await Note.find({}); 
+  const locals = {
+    title: "Dashboard",
+    description: "Free Notes App",
+  };
 
-      res.render('dashboard/index.ejs',{
-        userName:req.user.firstName,
-        locals,
-        notes,
-        layout:'../views/layouts/dashboard'})
+  try {
+    /* fetching a list of notes from a MongoDB database, sorting them by the updatedAt field in descending order, filtering them to include only notes belonging to a specific user, projecting only a subset of fields for each note */
 
-    } catch(error){
-      console.log(error);
-    }
+     const notes = await Note.aggregate([
+      { $sort: { updatedAt: -1 } },
+      { $match: { user:new mongoose.Types.ObjectId(req.user.id) } },
+      {
+        $project: {
+          title: { $substr: ["$title", 0, 30] },
+          body: { $substr: ["$body", 0, 100] },
+        },
+      },
+    ])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
 
+    const count = await Note.count(); 
 
-
-}
+    res.render('dashboard/index', {
+      userName: req.user.firstName,
+      locals,
+      notes,
+      layout: "../views/layouts/dashboard",
+      current: page,
+      pages: Math.ceil(count / perPage)
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
