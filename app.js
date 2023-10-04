b7339d9e92ec3a98f59fb771172b0adab4450c1c
JOEY <puiying526@gmail.com>
2023-10-04 22:35:28 +0100

    update

--- app.js
+++ app.js
@@ -1,44 +1,45 @@
 require('dotenv').config();
 
 const express = require('express');
-const methodOverride = require('method-override');
 const expressLayouts = require('express-ejs-layouts');
+const methodOverride = require("method-override");
 const connectDB = require('./server/config/db');
-const mongoose = require('mongoose');
-// Keep user login 
 const session = require('express-session');
 const passport = require('passport');
 const MongoStore = require('connect-mongo');
 
-const app = express(); 
+const app = express();
 const port = 3000 || process.env.PORT;
 
 app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true,
-    store: MongoStore.create({
-        mongoUrl: process.env.MONGODB_URI
-    })
-}))
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI
+  }),
+  //cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
+  // Date.now() - 30 * 24 * 60 * 60 * 1000
+}));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-app.use(express.urlencoded({extended:true}));
-app.use(express.json()); 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 app.use(methodOverride("_method"));
-// Connect to the Database 
-connectDB(); 
- 
+// Conntect to Database
+connectDB();  
+
 // Static Files
 app.use(express.static('public'));
 
 // Templating Engine
 app.use(expressLayouts);
-app.set('layout','./layouts/main');
-app.set('view engine','ejs');
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+
 
 // Routes
 app.use('/', require('./server/routes/auth'));
@@ -46,10 +47,12 @@ app.use('/', require('./server/routes/index'));
 app.use('/', require('./server/routes/dashboard'));
 
 // Handle 404
-app.get('*',(req,res)=> {
-    res.status(404).render('404.ejs');
+app.get('*', function(req, res) {
+  //res.status(404).send('404 Page Not Found.')
+  res.status(404).render('404');
 })
 
-app.listen(port,()=> {
-    console.log(`ON Port ${port}!`);
-})  
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
